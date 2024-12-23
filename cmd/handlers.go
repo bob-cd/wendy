c59af69740a5c4ac267e8c3444eadeb1e06954af
Rahul De <rahul080327@gmail.com>
2024-12-23 15:32:54 +0000

    [resource providers, artifact stores] handlers

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,31 +1,32 @@
 package cmd
 
 import (
-	"fmt"
 	"log/slog"
 
+	"github.com/bob-cd/wendy/cmd/handlers/artifact_stores"
 	"github.com/bob-cd/wendy/cmd/handlers/cluster"
 	"github.com/bob-cd/wendy/cmd/handlers/pipelines"
+	"github.com/bob-cd/wendy/cmd/handlers/resource_providers"
 	"github.com/lispyclouds/climate"
 	"github.com/spf13/cobra"
 )
 
-func handler(opts *cobra.Command, args []string, data climate.HandlerData) error {
-	slog.Info(fmt.Sprintf("Dummy hndler called with %+v\n", data))
+func placeHandler(opts *cobra.Command, args []string, data climate.HandlerData) error {
+	slog.Info("Oooh, you seem to have discovered a hidden one! I don't do much though!")
 	return nil
 }
 
 func GetHandlers() map[string]climate.Handler {
 	return map[string]climate.Handler{
-		"ArtifactStoreCreate":    handler,
-		"ArtifactStoreDelete":    handler,
-		"ArtifactStoreList":      handler,
+		"ArtifactStoreCreate":    artifact_stores.CreateHandler,
+		"ArtifactStoreDelete":    artifact_stores.DeleteHandler,
+		"ArtifactStoreList":      artifact_stores.ListHandler,
 		"ClusterInfo":            cluster.InfoHandler,
-		"GetApiSpec":             handler,
-		"GetError":               handler,
-		"GetEvents":              handler,
+		"GetApiSpec":             placeHandler,
+		"GetError":               placeHandler,
+		"GetEvents":              placeHandler,
 		"HealthCheck":            cluster.HealthCheckHandler,
-		"PipelineArtifactFetch":  handler,
+		"PipelineArtifactFetch":  placeHandler,
 		"PipelineCreate":         pipelines.CreateHandler,
 		"PipelineDelete":         pipelines.DeleteHandler,
 		"PipelineList":           pipelines.ListHandler,
@@ -36,9 +37,9 @@ func GetHandlers() map[string]climate.Handler {
 		"PipelineStatus":         pipelines.StatusHandler,
 		"PipelineStop":           pipelines.StopHandler,
 		"PipelineUnpause":        pipelines.UnpauseHandler,
-		"Query":                  handler,
-		"ResourceProviderCreate": handler,
-		"ResourceProviderDelete": handler,
-		"ResourceProviderList":   handler,
+		"Query":                  placeHandler,
+		"ResourceProviderCreate": resource_providers.CreateHandler,
+		"ResourceProviderDelete": resource_providers.DeleteHandler,
+		"ResourceProviderList":   resource_providers.ListHandler,
 	}
 }
